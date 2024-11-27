110f1640ed076917fc58f14a5ea70572989db86f
DyStorzion <benja2705@gmaail.com>
2024-11-27 17:39:20 -0300

    feat: se agrega implementacion para algoritmo optimo

--- algoritmos.cpp
+++ algoritmos.cpp
@@ -31,8 +31,64 @@ int simular_lru(const std::vector<int>& referencias, int numero_marcos) {
     return 0;
 }
 
+int encontrar_reemplazo_opt(const std::vector<int>& referencias, int indice) {
+    int pagina_a_remover = -1;
+    int max_distancia = -1;
+
+    std::vector<std::pair<int,int>> distancias;
+    std::vector<int> referencias_distintas;
+
+    for (int referencia : referencias) {
+        if(std::find(referencias_distintas.begin(), referencias_distintas.end(), referencia) == referencias_distintas.end()) {
+            referencias_distintas.push_back(referencia);
+        }
+    }
+
+    for(int i = 0; i < referencias_distintas.size(); i++) {
+        int distancia = 0;
+        for(int j = indice; j < referencias.size(); j++) {
+            distancia++;
+            if(referencias[j] == referencias_distintas[i]) {
+                break;
+            }
+
+        }
+        distancias.push_back({referencias_distintas[i], distancia});
+    }
+
+    for(auto par: distancias) {
+        if(par.second >= max_distancia) {
+            max_distancia = par.second;
+            pagina_a_remover = par.first;
+        }
+    }
+
+    return pagina_a_remover;
+}
+
 int simular_opt(const std::vector<int>& referencias, int numero_marcos) {
-    return 0;
+    hash_table_paginas marcos(numero_marcos);
+    int fallos_pagina = 0;
+    int ultimo_agregado;
+
+    for(int i = 0; i < referencias.size(); i++) {
+        if(!marcos.search(referencias[i])) {
+            fallos_pagina++;
+            if(marcos.size() == numero_marcos) {
+                int pagina_a_remover = encontrar_reemplazo_opt(referencias, i);
+                if(marcos.search(pagina_a_remover)) {
+                    marcos.remove(pagina_a_remover);
+                } else {
+                    pagina_a_remover = ultimo_agregado;
+                    marcos.remove(pagina_a_remover);
+                }
+            }
+            marcos.insert(referencias[i]);
+            ultimo_agregado = referencias[i];
+        } 
+    }
+
+    return fallos_pagina;
 }
 
 int simular_clock(const std::vector<int>& referencias, int numero_marcos) {
