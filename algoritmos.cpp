6e0ca3b511dbcc731195ca61076e7b545435ff25
lulunkaii <sofyiaolate@gmail.com>
2024-11-27 17:14:10 -0300

    feat: Se implementa algoritmo FIFO para parte 2

--- algoritmos.cpp
+++ algoritmos.cpp
@@ -7,7 +7,24 @@
 #include "hash_table_pair.h"
 
 int simular_fifo(const std::vector<int>& referencias, int numero_marcos) {
-    return 0;
+    hash_table_paginas hash_table(numero_marcos);
+    std::queue<int> cola;
+    int fallos_pagina = 0;
+
+    for (int i = 0; i < referencias.size(); i++) {
+        int ref = referencias[i];
+        if (!hash_table.search(ref)) {
+            fallos_pagina++;
+            if (cola.size() == numero_marcos) {
+                int pagina_eliminada = cola.front();
+                cola.pop();
+                hash_table.remove(pagina_eliminada);
+            }
+            cola.push(ref);
+            hash_table.insert(ref);
+        }
+    }
+    return fallos_pagina;
 }
 
 int simular_lru(const std::vector<int>& referencias, int numero_marcos) {
@@ -27,8 +44,6 @@ int simular_clock(const std::vector<int>& referencias, int numero_marcos) {
             fallos++;
             marcos.insert(referencias[i]);
         }
-        
-
     }
     return fallos;
 }
